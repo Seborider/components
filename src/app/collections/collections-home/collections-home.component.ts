c0c46b28467f2dba4ecc6b968f806c1344d4d1f1
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-05-24 17:20:45 +0200

    🤡 Add mock data

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'James', age: 24, job: 'designer' },
+    { name: 'Jill', age: 34, job: 'engineer' },
+    { name: 'Elyse', age: 32, job: 'designer' }
+        ];
+        
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', age: 'Age'},
+    { key: 'job', label: 'Job'}
+        ];
 
   constructor() { }
 
