52e8afbf0aeeed18a007b7cdc131e787f4ab26c4
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-05-24 17:40:31 +0200

    🤡 Add more mock data

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,14 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
   data = [
-    { name: 'James', age: 24, job: 'designer' },
-    { name: 'Jill', age: 34, job: 'engineer' },
-    { name: 'Elyse', age: 32, job: 'designer' }
+    { name: 'James', age: 24, job: 'designer', employed: true },
+    { name: 'Jill', age: 34, job: 'engineer', employed: true},
+    { name: 'Elyse', age: 32, job: 'designer', employed: true}
         ];
         
   headers = [
+    { key: 'employed', label: 'Has a job'},
     { key: 'name', label: 'Name' },
-    { key: 'age', age: 'Age'},
+    { key: 'age', label: 'Age'},
     { key: 'job', label: 'Job'}
         ];
 
