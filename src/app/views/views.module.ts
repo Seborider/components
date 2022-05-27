357442c3520cd7c8e672aa7c7c6223492bc8e610
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-05-27 12:08:54 +0200

    ✨ Create item list component

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -5,12 +5,14 @@ import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsHomeComponent } from './views-home/views-home.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { SharedModule } from '../shared/shared.module';
+import { ItemListComponent } from './item-list/item-list.component';
 
 
 @NgModule({
   declarations: [
     ViewsHomeComponent,
-    StatisticsComponent
+    StatisticsComponent,
+    ItemListComponent
   ],
   imports: [
     CommonModule,
