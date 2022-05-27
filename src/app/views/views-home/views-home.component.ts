ef83d5d61d68001fe1dc2b6aff22903383a6adb8
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-05-27 12:08:33 +0200

    🤡 Add mock data for images

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -10,7 +10,13 @@ export class ViewsHomeComponent implements OnInit {
     { value: 22, label: '# of Users'},
     { value: 900, label: 'Revenue'},
     { value: 50, label: 'Reviews'}
+  ];
+
+  items = [
+    { image: 'assets/images/couch.jpeg', title: 'Couch', description: 'This is a fantastic couch to sit on' },
+    { image: 'assets/images/dresser.jpeg', title: 'Dresser', description: 'This is a great dresser to put stuff in'}
   ]
+
 
   constructor() { }
 
