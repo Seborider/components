5417571b458124817a21a8887033a4d7d5306f5d
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-05-27 17:30:33 +0200

    🤡 Add mock data

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
   modalOpen = false
+  items = [
+    {title: 'Why is the sky black', content: 'The Sky is blue because it is'},
+    {title: 'Why is the sky purple', content: 'The Sky is blue purple it is'},
+    {title: 'Why is the sky blue', content: 'The Sky is black because it is'}
+  ]
   constructor() { }
 
   ngOnInit(): void {
